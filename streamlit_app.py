7137d9c57d9cf7153a2ea3ac4c606e1e854dd2b2
Marco Antonio Sandoval Cid <marco_sancid@comunidad.unam.mx>
2025-08-06 18:53:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ if ingredients_list:
 
         #st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+ fruit_chosen)
-# st.text(smoothiefroot_response.json())
+        # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # muestra la lista de frutas seleccionadas y alamacenadas en 
