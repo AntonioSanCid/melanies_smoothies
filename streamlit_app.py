d40331e5870fd12e658cac70aa0d3936fd1a3a4c
Marco Antonio Sandoval Cid <marco_sancid@comunidad.unam.mx>
2025-08-06 20:07:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 
 # para mostrar los datos de my_dataframe en formato de 
 # multiselección y no de tabla usamos:
