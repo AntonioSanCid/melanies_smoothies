9ce0d9d5b3df878c7b0c3519bebbfcdcacfcc9c6
Marco Antonio Sandoval Cid <marco_sancid@comunidad.unam.mx>
2025-08-06 20:08:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 # para mostrar los datos de my_dataframe en formato de 
@@ -91,6 +91,9 @@ if ingredients_list:
         # se agrega + ' ' para agregar un espacio entre cada selección
         # de frutas y no se vea amontonado
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
         # st.text(smoothiefroot_response.json())
